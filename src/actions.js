cf896798b9cb2e4a37eeba2fcdc1806050d96d06
Eric Schaefer <omg@eric-schaefer.com>
2018-03-28 20:52:43 +0200

    Tab fix

--- src/actions.js
+++ src/actions.js
@@ -1,37 +1,37 @@
 function actions(client) {
-	return {
-		apollo: {
-			setApolloData: value => state => {
-				return Object.assign({}, value);
-			},
-			queryWithApollo: value => async (state, actions) => {
-				const currentResult = client
-					.watchQuery({ query: value })
-					.currentResult();
-				const { loading, networkStatus, errors } = currentResult;
+  return {
+    apollo: {
+      setApolloData: value => state => {
+        return Object.assign({}, value);
+      },
+      queryWithApollo: value => async (state, actions) => {
+        const currentResult = client
+          .watchQuery({ query: value })
+          .currentResult();
+        const { loading, networkStatus, errors } = currentResult;
 
-				actions.setApolloData({ loading }); // test
+        actions.setApolloData({ loading }); // test
 
-				console.log("query info", value); // maybe save the component name somehow?
+        console.log("query info", value); // maybe save the component name somehow?
 
-				client.watchQuery({ query: value }).subscribe({
-					next: result => actions.setApolloData(result),
-					error: error => {}
-				});
-				// const thing = client.watchQuery({ query: value }).currentResult();
-				// console.log("thihng", thing);
+        client.watchQuery({ query: value }).subscribe({
+          next: result => actions.setApolloData(result),
+          error: error => {}
+        });
+        // const thing = client.watchQuery({ query: value }).currentResult();
+        // console.log("thihng", thing);
 
-				try {
-					// const result = await client.query({ query: value });
-					await client.query({ query: value });
+        try {
+          // const result = await client.query({ query: value });
+          await client.query({ query: value });
 
-					// actions.setApolloData(result);
-				} catch (e) {
-					console.log(e);
-				}
-			}
-		}
-	};
+          // actions.setApolloData(result);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+  };
 }
 
 export default actions;
