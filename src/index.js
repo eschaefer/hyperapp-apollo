ae77db8617088ab5e01cb0d1e0dc082ba6432594
Eric Schaefer <omg@eric-schaefer.com>
2018-03-28 21:02:35 +0200

    Syntax change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import resolveNode from "./resolveNode";
 import apolloActions from "./actions";
 
-const hyperappApollo = (client, app) => {
+function hyperappApollo(client, app) {
   return function(initialState, actionsTemplate, originalView, container) {
     const enhancedView = function(state, actions) {
       const node = resolveNode(originalView(state, actions), actions);
@@ -16,6 +16,6 @@ const hyperappApollo = (client, app) => {
 
     return app(initialState, enhancedActions, enhancedView, container);
   };
-};
+}
 
 export default hyperappApollo;
